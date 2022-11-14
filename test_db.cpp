f5756f1a7e56438a515f0242deefc1ba06830ddc
NaveenGangireddy <gangireddynaveen@gmail.com>
2022-11-14 11:42:55 +0530

    first commit

---
+++ test_db.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<sqlite3.h>
+int main(int argc, char** argv)
+{
+	sqlite3* DB;
+	int exit =0;
+	exit = sqlite3_open("example.db",&DB);
+
+	if (exit){
+		std::cerr<<"EError open DB "<<sqlite3_errmsg(DB)<<std::endl;
+		return(-1);
+	}
+	else
+		std::cout<<"Opened Database Successfully!"<<std::endl;
+	sqlite3_close(DB);
+	return (0);
+}
